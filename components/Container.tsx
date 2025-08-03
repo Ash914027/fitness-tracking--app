c8e3d5cf569c28d750bf2db7283f36a05c708081
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2025-08-03 17:44:30 +0530

    Update Container.tsx

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,5 +3,5 @@ export const Container = ({ children }: { children: React.ReactNode }) => {
   return <SafeAreaView className={styles.container}>{children}</SafeAreaView>;
 };
 const styles = {
-  container: 'flex flex-1 m-6',
+  container: 'flex flex-1 m-5',
 };
